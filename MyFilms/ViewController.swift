9e48d4113656792701aae70936841a1a86dc9fd8
Sophie Traynor <sophietraynor@Sophies-MacBook-Pro.local>
2017-11-02 10:31:18 +0000

    Initial Commit

---
+++ MyFilms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFilms
+//
+//  Created by Sophie Traynor on 02/11/2017.
+//  Copyright © 2017 Sophie Traynor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
